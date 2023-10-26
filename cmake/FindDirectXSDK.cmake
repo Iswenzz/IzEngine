c7c060e26cba5acb6a191fb403aa201153864850
Iswenzz <alexisnardiello@gmail.com>
2023-10-26 13:27:58 +0200

    Project: Added DirectX SDK for dx9

---
+++ cmake/FindDirectXSDK.cmake
@@ -0,0 +1,20 @@
+find_path(DXSDK_INCLUDE_DIRS "directxsdk/d3dx9.h")
+find_library(D3DX9_LIB d3dx9)
+
+set(DXSDK_LIBS
+	${D3DX9_LIB}
+	d3d9)
+
+set(DXSDK_INCLUDE_DIRS
+	${DXSDK_INCLUDE_DIRS}
+	${DXSDK_INCLUDE_DIRS}/directxsdk)
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(DirectXSDK DEFAULT_MSG DXSDK_LIBS DXSDK_INCLUDE_DIRS)
+mark_as_advanced(DXSDK_LIBS DXSDK_INCLUDE_DIRS)
+
+if(NOT TARGET DirectX::SDK)
+	add_library(DirectX::SDK INTERFACE IMPORTED)
+    set_property(TARGET DirectX::SDK PROPERTY INTERFACE_LINK_LIBRARIES "${DXSDK_LIBS}")
+	set_property(TARGET DirectX::SDK PROPERTY INTERFACE_INCLUDE_DIRECTORIES "${DXSDK_INCLUDE_DIRS}")
+endif()
