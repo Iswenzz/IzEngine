b4e5e9ee32086c69c92c85cc0da1661694d28aa4
Dualiteee <lordalite@gmail.com>
2024-02-03 21:53:54 +0100

    Graphics feature

---
+++ plugins/Graphics/main.cpp
@@ -0,0 +1,19 @@
+#include "Game/Plugin.hpp"
+#include "Features/Graphics.hpp"
+
+PLUGIN void Initialize(Plugin* plugin)
+{
+	plugin->SetInfos("sr.feature.graphics", "Graphics");
+
+	Features::Load<Graphics>();
+}
+
+PLUGIN void Renderer()
+{
+	GUI::ModuleContext();
+}
+
+PLUGIN void Shutdown()
+{
+	Features::Remove("sr.feature.graphics");
+}
