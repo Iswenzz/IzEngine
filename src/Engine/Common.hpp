30f64d00f90b4125be1e4b259294c170f87d7486
Alexis Nardiello <alexisnardiello@gmail.com>
2026-05-19 21:08:57 +0200

    Engine: Common

---
+++ src/Engine/Common.hpp
@@ -0,0 +1,17 @@
+#pragma once
+#include "Engine/Core/Application/Application.hpp"
+#include "Engine/Core/Common.hpp"
+#include "Engine/Core/Console/Console.hpp"
+#include "Engine/Core/IO/VFS.hpp"
+#include "Engine/Core/Memory/Memory.hpp"
+
+#include "Engine/Core/System/AssetManager.hpp"
+#include "Engine/Core/System/Crash.hpp"
+#include "Engine/Core/System/Environment.hpp"
+#include "Engine/Core/System/Plugin.hpp"
+#include "Engine/Core/System/System.hpp"
+#include "Engine/Core/System/ThreadPool.hpp"
+#include "Engine/Core/System/Window.hpp"
+
+#include "Engine/Backend/ImGUI/Common.hpp"
+#include "Engine/Renderer/Common.hpp"
--- src/Examples/WindowsDX9.cpp
+++ src/Examples/WindowsDX9.cpp
@@ -1,11 +1,4 @@
-#include "Engine/Core/Application/Application.hpp"
-#include "Engine/Core/Console/Console.hpp"
-#include "Engine/Core/System/Crash.hpp"
-#include "Engine/Core/System/Environment.hpp"
-#include "Engine/Core/System/Window.hpp"
-
-#include "Engine/Backend/ImGUI/Common.hpp"
-#include "Engine/Renderer/Common.hpp"
+#include "Engine/Common.hpp"
 
 void Application::Start()
 {
