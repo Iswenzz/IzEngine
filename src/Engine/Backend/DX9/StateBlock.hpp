88bdb4d3a7a5789ebb1c983aac3215006675fbfd
Alexis Nardiello <alexisnardiello@gmail.com>
2026-05-23 20:03:12 +0200

    DX9: StateBlock resource

---
+++ src/Engine/Backend/DX9/StateBlock.hpp
@@ -0,0 +1,24 @@
+#pragma once
+#include "Base.hpp"
+
+#include "Engine/Renderer/Base/GPUResource.hpp"
+
+namespace IzEngine
+{
+	class API DX9StateBlock : public GPUResource
+	{
+	public:
+		DX9StateBlock() = default;
+		~DX9StateBlock() override;
+
+		void Capture();
+		void Apply();
+
+		void Release() override;
+		void OnBeforeReset() override;
+		void OnAfterReset() override;
+
+	private:
+		IDirect3DStateBlock9* StateBlock = nullptr;
+	};
+}
