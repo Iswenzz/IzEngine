da9851e299ca477a55f742b7e262a6827dea24b9
Alexis Nardiello <alexisnardiello@gmail.com>
2026-08-30 23:54:58 +0200

    Assembler: Revert using namespace

--- src/Engine/Core/Memory/Assembler.hpp
+++ src/Engine/Core/Memory/Assembler.hpp
@@ -17,8 +17,6 @@ namespace arch = asmjit::aarch64;
 	#endif
 #endif
 
-using namespace asmjit;
-
 #define ASM_FUNCTION(function) void ASM_##function(asmjit::CodeHolder& code, arch::Assembler& a)
 #define ASM_LOAD(function) Assembler::NewCode(ASM_##function)
 #define ASM_TRAMPOLINE(hook) arch::ptr(uintptr_t(&hook.Trampoline))
